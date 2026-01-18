a8d5249af6c74ddca9d6e57ea3a97cdf20e1c5b2
Marqed <zacherywfrancis@gmail.com>
2026-01-18 18:59:08 -0500

    Done

--- Homework 13/src/BinaryTreeHomework.java
+++ Homework 13/src/BinaryTreeHomework.java
@@ -1,6 +1,7 @@
 import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BinaryTreeHomework {
 
@@ -29,9 +30,42 @@ public class BinaryTreeHomework {
         if (root == null) {
             return;
         }
-            list.add(root.data);
-            preorderListHelper(root.left, list);
-            preorderListHelper(root.right, list);
+        list.add(root.data);
+        preorderListHelper(root.left, list);
+        preorderListHelper(root.right, list);
+    }
+
+    public static <E> List<E> postorderList(BinaryTreeNode<E> root) {
+        List<E> list = new ArrayList<>();
+        postorderListHelper(root, list);
+        return list;
+    }
+
+    public static <E> List<E> inorderList(BinaryTreeNode<E> root) {
+        List<E> list = new ArrayList<>();
+        inorderListHelper(root, list);
+        return list;
+    }
+
+    public static <E> void inorderListHelper(BinaryTreeNode<E> root, List<E> list) {
+        if (root == null) {
+            return;
+        }
+
+        inorderListHelper(root.left, list);
+        list.add(root.data);
+        inorderListHelper(root.right, list);
+    }
+
+    public static <E> void postorderListHelper(BinaryTreeNode<E> root, List<E> list) {
+        if (root == null) {
+            return;
+        }
+
+        postorderListHelper(root.left, list);
+        postorderListHelper(root.right, list);
+
+        list.add(root.data);
     }
 
     //aka Breadth First
@@ -74,6 +108,51 @@ public class BinaryTreeHomework {
         return countLeavesHelper(root.left, nodeCount) + countLeavesHelper(root.right, nodeCount);
     }
 
+    public static <E> boolean isProper(BinaryTreeNode<E> root) {
+        if (root == null) {
+            return true;
+        }
+
+        boolean left = root.left != null;
+        boolean right = root.right != null;
+
+        if (left ^ right) {
+            return false;
+        }
+
+        return isProper(root.left) && isProper(root.right);
+    }
+
+    public static <E> boolean contains(BinaryTreeNode<E> root, E element) {
+        if (root == null) {
+            return false;
+        }
+
+        if (Objects.equals(root.data, element)) {
+            return true;
+        }
+
+        return contains(root.left, element) || contains(root.right, element);
+    }
+
+    public static int sum(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int value = (root.data == null) ? 0 : root.data;
+        return value + sum(root.left) + sum(root.right);
+    }
+
+    public static int maxPathSum(BinaryTreeNode<Integer> root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return root.data + Math.max(maxPathSum(root.left), maxPathSum(root.right));
+    }
+
     void main() {
         BinaryTreeNode<Integer> test_root = new BinaryTreeNode<>(15,
                 new BinaryTreeNode<>(3,
@@ -86,14 +165,58 @@ public class BinaryTreeHomework {
                                 new BinaryTreeNode<>(null), new BinaryTreeNode<>(null)),
                         new BinaryTreeNode<>(2,
                                 new BinaryTreeNode<>(null), new BinaryTreeNode<>(9,
-                                new BinaryTreeNode<>(null), new BinaryTreeNode<>(12))))); // 10 nodes total
+                                new BinaryTreeNode<>(null), new BinaryTreeNode<>(12))))); // 10 nodes total, 9 leaves
 
         System.out.println(countLeaves(test_root));
 
-        List<Integer> list = new ArrayList<>();
-        list = preorderList(test_root);
+        List<Integer> list1 = new ArrayList<>();
+        List<List<Integer>> list2 = new ArrayList<>();
 
-        System.out.println(list);
+        //preorder
+        list1 = preorderList(test_root);
+        System.out.println("preorder: " + "\n" + list1 + "\n");
 
+        //inorder
+        list1 = inorderList(test_root);
+        System.out.println("inorder: " + "\n" + list1 + "\n");
+
+        //postorder
+        list1 = postorderList(test_root);
+        System.out.println("postorder: " + "\n" + list1 + "\n");
+
+        //level-order
+        list2 = levelOrderList(test_root);
+        System.out.println("level-order: " + "\n" + list2 + "\n");
+
+        //count leaves
+        System.out.println("count leaves: " + countLeaves(test_root) + "\n"); //9
+
+        BinaryTreeNode<Integer> improper_root = new BinaryTreeNode<>(5,
+                new BinaryTreeNode<>(10,
+                        new BinaryTreeNode<>(null),
+                        null),
+                new BinaryTreeNode<>(15,
+                        new BinaryTreeNode<>(null),
+                        new BinaryTreeNode<>(null)));
+        //Note:
+        //new BinaryTree<>(null); is still valid child
+        //null is invalid as the branch totally doesn't exist
+
+        System.out.println("count leaves: " + countLeaves(test_root) + "\n"); //9
+
+        //isProper
+        System.out.println(isProper(test_root)); //true
+        System.out.println(isProper(improper_root)); //false
+
+        IO.println();
+
+        //contains
+        System.out.println(contains(test_root, 15)); //true
+        System.out.println(contains(test_root, 12)); //true
+        System.out.println(contains(test_root, 0)); //false
+
+        IO.println();
+
+        System.out.println("sum of binary tree: " + sum(improper_root) + "\n"); //30
     }
 }
