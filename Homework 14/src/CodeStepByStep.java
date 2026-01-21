3a1f95131503e005283463942cf8306f849f3ec9
Marqed <Derpexsplosion@gmail.com>
2026-01-20 19:02:20 -0500

    A little bit verbose.

--- Homework 14/src/CodeStepByStep.java
+++ Homework 14/src/CodeStepByStep.java
@@ -1,31 +1,34 @@
-import com.sun.source.tree.Tree;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.TreeMap;
-import java.util.Scanner;
-import java.util.Map;
+import java.util.*;
 import java.io.File;
 
 public class CodeStepByStep {
 
     //notes:
     //Names must appear in alphabetical order...
+    //Can't open file more than once.
 
     public static String biggestFamily(String fileInput) throws IOException {
         Scanner scan = new Scanner(new File(fileInput));
         TreeMap<String, Integer> fam = new TreeMap<>();
+        List<String> largestFamList = new LinkedList<>();
+        List<String> famMembers = new LinkedList<>();
 
         while (scan.hasNextLine()) {
             String person = scan.nextLine();
-            String familyName = person.substring
-                    (person.indexOf(" ") + 1);
+            famMembers.add(person);
 
-            fam.put(familyName, fam.getOrDefault(familyName, 0) + 1);
+            fam.put(person.substring
+                    (person.indexOf(" ") + 1), fam.getOrDefault(person.substring
+                    (person.indexOf(" ") + 1), 0) + 1);
         }
 
-        ArrayList<String> largestFamList = new ArrayList<>();
+        scan.close();
 
+        Collections.sort(famMembers);
+        Queue<String> peopleFromFile = new LinkedList<>(famMembers);
+
+        //find the largest family
         int largestFamCount = 0;
         for (Map.Entry<String, Integer> entry : fam.entrySet()) {
             if (entry.getValue() > largestFamCount) {
@@ -33,37 +36,39 @@ public class CodeStepByStep {
             }
         }
 
+        //add largest families
         for (Map.Entry<String, Integer> entry : fam.entrySet()) {
             if (entry.getValue() == largestFamCount) {
                 largestFamList.add(entry.getKey());
             }
         }
 
-        StringBuilder largestFamPrint = new StringBuilder();
-        int index = 0;
-
+        //run through list of largest families
         while (!largestFamList.isEmpty()) {
 
-            largestFamPrint.append(largestFamList.get(index) + " family: ");
+            System.out.print(largestFamList.get(0) + " family: ");
+            int index2 = peopleFromFile.size();
 
-            scan = new Scanner(new File(fileInput));
+            //run through list of available names
+            while (index2 > 0) {
+                index2--;
 
-            while (scan.hasNextLine()) {
-                String person = scan.nextLine();
-                if (largestFamList.get(0).equals((person.substring
-                        (person.indexOf(" ") + 1)))) {
+                //remove/add people from queue to preserve data struct
+                String person = peopleFromFile.remove();
+                peopleFromFile.add(person);
 
-                    largestFamPrint.append(person.substring(0, person.indexOf(" ")) + " ");
-                }
-
-                if (!scan.hasNextLine()) {
-                    System.out.println(largestFamPrint);
-                    largestFamPrint = new StringBuilder();
-                    largestFamList.remove(0);
+                //compare & print those with the CURRENTLY INDEXED family name
+                if (largestFamList.get(0).equals((person.substring(person.indexOf(" ") + 1)))) {
+                    if (!(index2 - 1 < 0)) {
+                        System.out.print(person.substring(0, person.indexOf(" ")) + " ");
+                    } else {
+                        System.out.print(person.substring(0, person.indexOf(" ")));
+                    }
                 }
             }
+            largestFamList.remove(0);
+            System.out.println();
         }
-        scan.close();
         return "";
     }
 
